832f014d36c27a232032e608942736c9fbb90a16
theo v <theo.viardin1@etu.univ-nantes.fr>
2019-10-07 19:16:31 +0200

    Test sur l'api twitter

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const https = require('https')
 const fs = require('fs')
+const Twitter = require('twitter')
 
 const app = express()
 const q = faunadb.query
@@ -11,9 +12,16 @@ const q = faunadb.query
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var client = new faunadb.Client({secret: 'fnADY9xgfRACAurCr5ZpzFsFqRuteoZ8daFBymq-'})
+var clientFauna = new faunadb.Client({secret: 'fnADY9xgfRACAurCr5ZpzFsFqRuteoZ8daFBymq-'})
 
-/*var createP = client.query(
+var clientTwitter = new Twitter({
+    consumer_key: '6d39TkEgSHYGk7ttA1barye7P',
+    consumer_secret: '1BNWHSyvjQtx5yYg1RNgXjQm9lXS7nODVBlxv0Osu8QxEQEZuG',
+    access_token_key: '1181222020899643392-aDIqDiO6l6nWywrZZkUS67VcNerjbN',
+    access_token_secret: 'XUgcMioxWCp5vzlerSb0KPqhakB2Cx5xJjXs7IZB6x0Wo'
+});
+
+/*var createP = clientFauna.query(
     q.Create(
         q.Collection('user'), 
         { data: 
@@ -30,8 +38,77 @@ createP.then(function(response) {
     console.log(response.ref); // Would log the ref to console.
 });*/
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {    
     res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.get('/sigma.min.js', function (req, res) {   
+    res.sendFile(path.join(__dirname + '/sigma.js/build/sigma.min.js'));
+})
+
+app.post('/', function(req, res) {
+    var params = {screen_name: req.body.screen_name};
+    
+    clientTwitter.get('users/show', params, function(error, user1, response) {
+        if (!error) {
+            var createUser1 = clientFauna.query(
+                q.Create(
+                    q.Collection('user'), 
+                    { data: 
+                        {
+                            id: user1.id,
+                            name: user1.screen_name,
+                            user_created_date: user1.created_at
+                        } 
+                    }
+                )
+            );
+            createUser1.then(function (response) {
+                console.log(response)
+                clientTwitter.get('followers/list', params, function(error, users2, response) {
+                    if (!error) {
+                    
+                        users2.users.map((user2, index) => {            
+                            var createUser = clientFauna.query(
+                                q.Create(
+                                    q.Collection('user'), 
+                                    { data: 
+                                        {
+                                            id: user2.id,
+                                            name: user2.screen_name,
+                                            user_created_date: user2.created_at
+                                        } 
+                                    }
+                                )
+                            );
+                            
+                            createUser.then(function (response) {
+                                var createUser_user = clientFauna.query(
+                                    q.Create(
+                                        q.Collection('user_user'), 
+                                        { data: 
+                                            {
+                                                user1: user1.id,
+                                                user2: user2.id
+                                            } 
+                                        }
+                                    )
+                                );
+                            })
+                        })
+                        console.log(users2)
+                        res.json({
+                            user1: user1,
+                            users2: users2.users
+                        })
+                    }
+                });
+            })
+
+            
+        }
+    })
+    
 });
 
 app.post('/user', function (req, res) {
@@ -40,7 +117,7 @@ app.post('/user', function (req, res) {
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     var dateTime = date+' '+time;
 
-    var createUser = client.query(
+    var createUser = clientFauna.query(
         q.Create(
             q.Collection('user'), 
             { data: 
@@ -58,9 +135,7 @@ app.post('/user', function (req, res) {
     });
 })
 
-https.createServer({
-    key: fs.readFileSync('key.pem'),
-    passphrase: 'theoviardin',
-    cert: fs.readFileSync('cert.pem')
-}, app).listen(443)
+app.listen(3000, 'localhost', function () {
+    console.log('listening on port 3000')
+})
 
